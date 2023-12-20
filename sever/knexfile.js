1a8aaf8725e7c904ea8f720705f27d0cabc095c6
navin972 <sandula.navin@gmail.com>
2023-12-20 08:34:02 +0530

    model view controller

--- sever/knexfile.js
+++ sever/knexfile.js
@@ -2,7 +2,7 @@
 module.exports = {
   development: {
     client: "pg",
-    connection: process.env.DATABASE_URL || {
+    connection: {
       host: "localhost",
       user: "postgres",
       password: "navin@345",
